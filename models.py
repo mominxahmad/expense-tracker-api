84e101877e904b9e70f187c3788663e9f3322a0a
Momin <mominahmad2004@gmail.com>
2026-09-05 23:53:11 +0500

    Expense Model setup

--- models.py
+++ models.py
@@ -1,14 +1,25 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
     username = Column(String, unique=True, nullable=False)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
-    role = Column(String, default='User', nullable=False)
+    role = Column(String, default='user', nullable=False)
+
+
+class Expense(Base):
+    __tablename__ = "expense"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    amount = Column(Integer, nullable=False)
+    category = Column(String, nullable=False)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
