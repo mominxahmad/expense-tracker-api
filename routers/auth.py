2b2929caf3088f64af05523412fe50ee65bf13c0
Momin <mominahmad2004@gmail.com>
2026-09-06 01:31:59 +0500

    JWT Login Endpoint

--- routers/auth.py
+++ routers/auth.py
@@ -6,8 +6,19 @@ from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 from models import User
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import datetime, timezone , timedelta
+from jose import jwt
+import os
+from dotenv import load_dotenv
+
 
 router = APIRouter(prefix="/auth", tags=["Authorization"])
+
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 
 def get_db():
@@ -31,7 +42,34 @@ class CreateUserModel(BaseModel):
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
-@router.post("/register",status_code=status.HTTP_201_CREATED)
+Oauth_dependency=Annotated[OAuth2PasswordRequestForm, Depends()]
+
+
+def confirm_and_get_user(username: str, plain_password: str, db: Session):
+    user = db.query(User).filter(User.username==username).first()
+    if user is None:
+        return False
+    if not bcrypt_context.verify(plain_password, user.hashed_password):
+        return False
+    return user
+
+
+def assign_token(id: int, username: str, role: str, time_delta: timedelta):
+    encode = {
+        "sub": username,
+        "id": id,
+        "role": role
+    }
+    expires = datetime.now(timezone.utc) + time_delta
+    encode.update({"exp":expires})
+    return jwt.encode(encode,SECRET_KEY,ALGORITHM)
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+@router.post("/register", status_code=status.HTTP_201_CREATED)
 async def create_user(db: database_dependency, user_data: CreateUserModel):
     new_user = User(
         email = user_data.email,
@@ -42,3 +80,15 @@ async def create_user(db: database_dependency, user_data: CreateUserModel):
     )
     db.add(new_user)
     db.commit()
+
+
+@router.post("/login", response_model=Token)
+async def login_for_access_token(db: database_dependency, form_data: Oauth_dependency):
+    user = confirm_and_get_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid Credentials")
+    token = assign_token(user.id, user.username, user.role, timedelta(minutes=30))
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
