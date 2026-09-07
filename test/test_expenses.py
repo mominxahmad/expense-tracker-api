7a96debfa6afe936b3bc2fa0fb08597aca6abe6b
Momin <mominahmad2004@gmail.com>
2026-09-08 03:58:42 +0500

    Tests for Read Expenses

---
+++ test/test_expenses.py
@@ -0,0 +1,37 @@
+from test.utils import *
+from routers.expenses import get_db, authenticate_current_user
+from fastapi import status
+
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[authenticate_current_user] = override_authenticate_current_user
+
+
+def test_get_all_expenses(test_expense):
+    response = client.get("/expenses/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [{
+        "id" : 1,
+        "title" : "gym membership",
+        "amount" : 5000,
+        "category" : "subscriptions",
+        "description" : "Uni sept membership fee for the gym",
+        "user_id" : 1
+    }]
+
+
+def test_get_expense_by_id(test_expense):
+    response = client.get("/expenses/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "id" : 1,
+        "title" : "gym membership",
+        "amount" : 5000,
+        "category" : "subscriptions",
+        "description" : "Uni sept membership fee for the gym",
+        "user_id" : 1
+    }
+def test_get_expense_by_INVALID_id(test_expense):
+    response = client.get("/expenses/99")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == { "detail" : "Expense Not Found" }
