d8aecd0ced6c001df0ebfeb5b97a78be40e1c1bb
Momin <mominahmad2004@gmail.com>
2026-09-16 02:09:35 +0500

    Migrate expense router to async SQLAlchemy

--- routers/expenses.py
+++ routers/expenses.py
@@ -1,24 +1,18 @@
 from fastapi import APIRouter, Depends, Path, HTTPException
 from pydantic import BaseModel, Field
-from database import SessionLocal
+from database import get_db
 from typing import Annotated
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from routers.auth import authenticate_current_user
 from models import Expense,User
 from starlette import status
+from sqlalchemy import select
 
 
 router = APIRouter(prefix="/expenses", tags=["Expenses"])
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-database_dependency = Annotated[Session, Depends(get_db)]
+database_dependency = Annotated[AsyncSession, Depends(get_db)]
 
 user_dependency = Annotated[dict, Depends(authenticate_current_user)]
 
@@ -32,12 +26,19 @@ class CreateExpenseModel(BaseModel):
 
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_all_expenses(db: database_dependency, user: user_dependency):
-    return db.query(Expense).filter(Expense.user_id==user.get("id")).all()
+    response = await db.execute(
+        select(Expense).where(Expense.user_id==user.get("id"))
+    )
+    return response.scalars().all()
 
 
 @router.get("/{expense_id}", status_code=status.HTTP_200_OK)
 async def get_expense_by_id(db: database_dependency, user: user_dependency, expense_id: int = Path(gt=0)):
-    expense_to_return = db.query(Expense).filter(Expense.id==expense_id).filter(Expense.user_id==user.get("id")).first()
+    response = await db.execute(
+        select(Expense).where(Expense.user_id==user.get("id"),
+                              Expense.id==expense_id)
+    )
+    expense_to_return = response.scalar_one_or_none()
     if expense_to_return is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense Not Found")
     return expense_to_return
@@ -47,14 +48,18 @@ async def get_expense_by_id(db: database_dependency, user: user_dependency, expe
 async def create_new_expense(db: database_dependency, user: user_dependency, new_expense: CreateExpenseModel):
     new_expense_model = Expense(**new_expense.model_dump(),user_id=user.get("id"))
     db.add(new_expense_model)
-    db.commit()
-    db.refresh(new_expense_model)
+    await db.commit()
+    await db.refresh(new_expense_model)
     return new_expense_model
 
 
 @router.put("/{expense_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_expense_by_id(db: database_dependency, user: user_dependency, update_model: CreateExpenseModel, expense_id: int = Path(gt=0)):
-    task_to_update = db.query(Expense).filter(Expense.id==expense_id).filter(Expense.user_id==user.get("id")).first()
+    response = await db.execute(
+        select(Expense).where(Expense.id==expense_id,
+                              Expense.user_id==user.get("id"))
+    )
+    task_to_update = response.scalar_one_or_none()
     if task_to_update is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense Not Found")
     task_to_update.title = update_model.title
@@ -62,13 +67,17 @@ async def update_expense_by_id(db: database_dependency, user: user_dependency, u
     task_to_update.amount = update_model.amount
     task_to_update.description = update_model.description
     db.add(task_to_update)
-    db.commit()
+    await db.commit()
 
 
 @router.delete("/{expense_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_expense_by_id(db: database_dependency, user: user_dependency, expense_id: int = Path(gt=0)):
-    task_to_delete = db.query(Expense).filter(Expense.id==expense_id).filter(Expense.user_id==user.get("id")).first()
+    response = await db.execute(
+        select(Expense).where(Expense.id==expense_id,
+                              Expense.user_id==user.get("id"))
+    )
+    task_to_delete = response.scalar_one_or_none()
     if task_to_delete is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense Not Found")
-    db.delete(task_to_delete)
-    db.commit()
+    await db.delete(task_to_delete)
+    await db.commit()
