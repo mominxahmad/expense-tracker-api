8d06d0ad35c4694dcaf243b13cf0cdb3c4fea41d
Momin <mominahmad2004@gmail.com>
2026-09-16 02:28:02 +0500

    Migrate admin router to async SQLAlchemy

--- routers/admin.py
+++ routers/admin.py
@@ -1,24 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException, Path
-from database import SessionLocal
+from database import get_db
 from typing import Annotated, Literal
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from routers.auth import authenticate_current_user
 from starlette import status
 from models import User, Expense
 from pydantic import BaseModel, Field
+from sqlalchemy import select
 
 
 router = APIRouter(prefix="/admin", tags=["Admin"])
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-database_dependency = Annotated[Session, Depends(get_db)]
+database_dependency = Annotated[AsyncSession, Depends(get_db)]
 
 user_dependency = Annotated[dict, Depends(authenticate_current_user)]
 
@@ -48,13 +42,17 @@ class UserRoleModel(BaseModel):
 
 @router.get("/users", response_model=list[UserResponseModel], status_code=status.HTTP_200_OK)
 async def get_all_users(db: database_dependency, admin: admin_dependency):
-    users_to_return = db.query(User).all()
+    response = await db.execute(select(User))
+    users_to_return = response.scalars().all()
     return users_to_return
 
 
 @router.get("/users/{user_id}", response_model=UserResponseModel, status_code=status.HTTP_200_OK)
 async def get_user_by_id(db: database_dependency, admin: admin_dependency, user_id: int = Path(gt=0)):
-    user_to_return = db.query(User).filter(User.id==user_id).first()
+    response = await db.execute(
+        select(User).where(User.id==user_id)
+    )
+    user_to_return = response.scalar_one_or_none()
     if user_to_return is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="User Not Found")
@@ -65,39 +63,54 @@ async def get_user_by_id(db: database_dependency, admin: admin_dependency, user_
             status_code=status.HTTP_200_OK)
 async def update_user_role(db: database_dependency, admin: admin_dependency,
                            user_role: UserRoleModel,user_id: int = Path(gt=0)):
-    user_to_update = db.query(User).filter(User.id==user_id).first()
+    response = await db.execute(
+        select(User).where(User.id==user_id)
+    )
+    user_to_update = response.scalar_one_or_none()
     if user_to_update is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="User Not Found")
     user_to_update.role = user_role.role
-    db.commit()
-    db.refresh(user_to_update)
+    await db.commit()
+    await db.refresh(user_to_update)
     return user_to_update
 
 
 @router.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user_by_id(db: database_dependency, admin: admin_dependency, user_id: int = Path(gt=0)):
-    user_to_delete = db.query(User).filter(User.id==user_id).first()
+    response = await db.execute(
+        select(User).where(User.id==user_id)
+    )
+    user_to_delete = response.scalar_one_or_none()
     if user_to_delete is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="User Not Found")
-    expenses_to_delete = db.query(Expense).filter(Expense.user_id==user_id).all()
+    responses = await db.execute(
+        select(Expense).where(Expense.user_id==user_id)
+    )
+    expenses_to_delete = responses.scalars().all()
     for expense in expenses_to_delete:
-        db.delete(expense)
-    db.delete(user_to_delete)
-    db.commit()
+        await db.delete(expense)
+    await db.delete(user_to_delete)
+    await db.commit()
 
 
 @router.get("/expenses", status_code=status.HTTP_200_OK)
 async def get_all_expenses(db: database_dependency, admin: admin_dependency):
-    expenses_to_return = db.query(Expense).all()
+    response = await db.execute(
+        select(Expense)
+    )
+    expenses_to_return = response.scalars().all()
     return expenses_to_return
 
 
 @router.get("/expenses/{expense_id}", status_code=status.HTTP_200_OK)
 async def get_expense_by_expense_id(db: database_dependency, admin: admin_dependency,
                                     expense_id: int = Path(gt=0)):
-    expense_to_return = db.query(Expense).filter(Expense.id==expense_id).first()
+    response = await db.execute(
+        select(Expense).where(Expense.id==expense_id)
+    )
+    expense_to_return = response.scalar_one_or_none()
     if expense_to_return is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Expense Not Found")
@@ -107,20 +120,29 @@ async def get_expense_by_expense_id(db: database_dependency, admin: admin_depend
 @router.get("/expenses/user/{user_id}", status_code=status.HTTP_200_OK)
 async def get_expenses_by_user_id(db: database_dependency, admin: admin_dependency,
                                     user_id: int = Path(gt=0)):
-    check_user_exists = db.query(User).filter(User.id==user_id).first()
+    response = await db.execute(
+        select(User).where(User.id==user_id)
+    )
+    check_user_exists = response.scalar_one_or_none()
     if check_user_exists is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="User Not Found")
-    expenses_to_return = db.query(Expense).filter(Expense.user_id==user_id).all()
+    responses = await db.execute(
+        select(Expense).where(Expense.user_id==user_id)
+    )
+    expenses_to_return = responses.scalars().all()
     return expenses_to_return
 
 
 @router.delete("/expenses/{expense_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_expense_by_expense_id(db: database_dependency, admin: admin_dependency,
                                        expense_id: int = Path(gt=0)):
-    expense_to_delete = db.query(Expense).filter(Expense.id==expense_id).first()
+    response = await db.execute(
+        select(Expense).where(Expense.id==expense_id)
+    )
+    expense_to_delete = response.scalar_one_or_none()
     if expense_to_delete is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Expense Not Found")
-    db.delete(expense_to_delete)
-    db.commit()
+    await db.delete(expense_to_delete)
+    await db.commit()
