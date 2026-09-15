c9c7c7f51527f918f5739b8f0424018d6017e7a8
Momin <mominahmad2004@gmail.com>
2026-09-16 00:40:01 +0500

    Migrate database configuration to async SQLAlchemy

--- database.py
+++ database.py
@@ -1,13 +1,18 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-load_dotenv()
 
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+async_engine = create_async_engine(DATABASE_URL, pool_pre_ping=True)
+async_SessionLocal = async_sessionmaker(bind=async_engine,expire_on_commit=False)
 Base = declarative_base()
+
+
+async def get_db():
+    async with async_SessionLocal() as db:
+        yield db
